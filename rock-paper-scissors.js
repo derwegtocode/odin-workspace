dac1a1f8880ed6ed08030c22757d30b066f0809d
Maria Lujan Sosa Balsa <luubalsa@gmail.com>
2022-07-20 15:47:22 +0200

    agregar eventlistener a botones, funcion de jugar ok, aparecen las selecciones de cada uno.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,105 +1,85 @@
+const OPTIONS = ["rock", "paper", "scissors"];
 
-const selectionButtons = document.querySelectorAll('[data-selection]');
-const finalColumn = document.querySelector('[data-final-column]')
-
-
-const SELECTIONS = [
-    {
-        name: "rock",
-        beats: "scissors",
-    },
-    {
-        name: "paper",
-        beats: "rock",
-    },
-    {
-        name: "scissors",
-        beats: "paper",
-    },
-];
+let computerSelection
 
 function computerPlay() {
-    let computerChoice = Math.floor(Math.random() * SELECTIONS.length);
-    return SELECTIONS[computerChoice];
+    let computerChoice = Math.floor(Math.random() * OPTIONS.length);
+    computerSelection = computerChoice
+    computerChoiceDisplay.textContent =  OPTIONS[computerChoice];
 }
 
-selectionButtons.forEach(selectionButton => {
-    selectionButton.addEventListener('click', e => {
-        const selectionName = selectionButton.dataset.selection
-        const selection = SELECTIONS.find(selection => selection.name === selectionName)
-        makeSelection(selection)
-    });
-});
-
-let playerScore = 0;
-let computerScore = 0;
-
-
-function makeSelection(selection) {
-    const computerSelection = computerPlay()
-    const youreWinner = isWinner(selection, computerSelection)
-    const computerWinner = isWinner(computerSelection, selection)
-    
-    addSelectionResult(computerSelection, computerWinner)
-    addSelectionResult(selection, youreWinner)
-
-    if (youreWinner) {
-        playerScore++
-        }
-    else if (computerWinner) {
-        computerScore++
-    }
-    else {
-        playerScore
-        computerScore
-    }
-    
-}
-
-
-function addSelectionResult(selection, winner) {
-    const div = document.createElement('div')
-    div.innerText = selection.name
-    div.classList.add('result-selection')
-    if (winner) div.classList.add('winner')
-    finalColumn.after(div)
-    document.getElementById('result-score-player').innerHTML = playerScore
-    document.getElementById('result-score-computer').innerHTML = computerScore
-}
-
-function isWinner(selection, opponentSelection) {
-    return selection.beats === opponentSelection.name
-}
-
-
-
-
-/*
 let computerScore = 0;
 let playerScore = 0;
+
+const userChoiceDisplay = document.createElement('p');
+const computerChoiceDisplay = document.createElement('p');
+const yourScore = document.createElement('p');
+const machineScore = document.createElement('p');
+const gameGrid = document.getElementById('games');
+const scoreGrid = document.getElementById('points');
+const messageForUser = document.createElement('p');
+const messageDiv = document.getElementById('message');
+
+messageDiv.append(messageForUser);
+
+gameGrid.append(userChoiceDisplay, computerChoiceDisplay);
+scoreGrid.append(yourScore, machineScore);
+
+function updateChoices() {
+    userChoiceDisplay.textContent = playerSelection
+    computerChoiceDisplay.textContent = computerSelection
+}
+
+
+
+console.log(updateScores())
+
+let playerSelection;
+
+const handleClick = (e) => {
+    playerSelection = e.target.id
+    userChoiceDisplay.textContent = playerSelection
+    computerPlay();
+    
+}
+
+for (let i = 0; i < OPTIONS.length; i++) {
+    document.getElementById(OPTIONS[i]).addEventListener('click', handleClick)
+}
+
+
+
 
                 
 function playRound() {
-    var computerSelection = computerPlay();
-    var playerSelectionPrompted = prompt("Choose wisely...");
-    var playerSelection = playerSelectionPrompted.toUpperCase();
-
+   
+    
     if (computerSelection == playerSelection) {
         console.log(computerSelection);
-         return "It's a tie!"
+        updateScores()
+        messageForUser.textContent = "It's a tie!"
     }
-    else if (computerSelection == "ROCK" && playerSelection == "SCISSORS" || computerSelection == "PAPER" && playerSelection == "ROCK" || computerSelection == "SCISSORS" && playerSelection == "PAPER") {
+    else if (computerSelection == rock && playerSelection == "scissors" || 
+        computerSelection == paper && playerSelection == "rock" || 
+        computerSelection == scissors && playerSelection == "paper") {
         console.log(computerSelection);
         computerScore++;
-        return ("You lose! " + computerSelection + " beats " + playerSelection + "!!");
+        updateScores()
+        messageForUser.textContent = ("You lose! " + computerSelection + " beats " + playerSelection + "!!");
     }
     else {
         console.log(computerSelection);
         playerScore++;
-        return ("You win! " + playerSelection + " beats " + computerSelection + "!!");
+        updateScores()
+        messageForUser.textContent = ("You win! " + playerSelection + " beats " + computerSelection + "!!");
     };
 };
-*/
+
+function updateScores() {
+    yourScore.textContent = ('YOU: ' + playerScore)
+    machineScore.textContent = ('THE MACHINE: ' + computerScore )
+}
+
 /* 5 juegos */
 
 /*
