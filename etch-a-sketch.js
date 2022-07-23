039f2632e50cbee3e212d40a298a493fc94a6c85
Maria Lujan Sosa Balsa <luubalsa@gmail.com>
2022-07-23 02:34:36 +0200

    proyecto terminado completamente funcional

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,21 +1,60 @@
+let color = 'black';
+let background = 'white';
+let click = false;
+
 function createGrid(size) {
-    let grid = document.querySelector('#gridDiv');
-    let squares = grid.querySelectorAll('div')
+    let myGrid = document.querySelector('#gridDiv');
+    let squares = myGrid.querySelectorAll('div')
     squares.forEach((div) => div.remove());
-    grid.style.gridTemplateColumns = `repeat(${size}, 1 fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1 fr)`;
+    myGrid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    myGrid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     let amount = size * size;
 
     for (let i = 0; i < amount; i++) {
         let oneSquare = document.createElement('div');
-        oneSquare.addEventListener('mouseover', () => {
-            oneSquare.style.backgroundColor = "black"});
-        oneSquare.style.backgroundColor = 'white';
-        grid.insertAdjacentElement('beforeend', oneSquare);
+        oneSquare.addEventListener('mouseover', colorSquare);
+        oneSquare.style.backgroundColor = background;
+        myGrid.insertAdjacentElement('beforeend', oneSquare);
+        
     };
 };
+createGrid (16);
 
 function changeSize(input) {
     createGrid(input);
 };
+
+function colorSquare() {
+    if(click) {
+        if (color == 'random') {
+            randomArctic()
+        }
+        else {
+            this.style.backgroundColor = color;
+        }  
+    };
+};
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function arcticGrid(cleanBoard) {
+    let myGrid = document.querySelector('#gridDiv');
+    let squares = myGrid.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = cleanBoard);
+};
+
+document.getElementById('colorSelector').oninput = (e) => changeColor(e.target.value);
+
+
+function randomArctic() {
+    color = 'rgb(50, 50, ' + (Math.floor(Math.random() * 256)) + ')';
+};
+
+/* making this work only when the click button is down */
+
+document.querySelector('#gridDiv').addEventListener('click', () => {
+    click = !click;
+})
