d9ef111437cf42aa975930dce2c00bd228c963db
Maria Lujan Sosa Balsa <luubalsa@gmail.com>
2022-08-05 12:11:43 +0200

    randomizador funcionando

---
+++ stock_picker.rb
@@ -0,0 +1,9 @@
+#assigning random values to 30 days 
+stocks = []
+30.times { stocks.push(rand(1..100)) }
+p stocks
+
+def stock_picker(stocks)
+  #choosing the best profit from best buy and consequent best sell
+
+end
