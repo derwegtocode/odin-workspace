2f16c6f83f6eb97630471c79d6da49da7c4bcc3f
Maria Lujan Sosa Balsa <luubalsa@gmail.com>
2022-09-01 13:30:11 +0200

    proyecto terminado

--- project_recursion.rb
+++ project_recursion.rb
@@ -34,3 +34,28 @@ def fibs_rec(n, fibs_array = [0, 1])
 end
 
 p fibs_rec(9)
+
+
+
+#Build a method #merge_sort that takes in an array 
+#and returns a sorted array, using a recursive merge sort methodology.
+
+#creating random array
+random_array = []
+(rand(5..10)).times { random_array.push(rand(1..100)) }
+p random_array
+
+
+def merge_sort(a)
+  return a if a.length < 2
+  b = merge_sort(a[0..((a.length / 2) - 1)])
+  c = merge_sort(a[(a.length / 2)..(a.length)])
+  d = []
+  until b.empty? || c.empty?
+    b.first <= c.first ? d << b.shift : d << c.shift
+  end
+  d + b + c
+
+end
+
+p merge_sort(random_array)
